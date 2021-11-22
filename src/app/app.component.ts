ab0da6176bb8747d175ddae9159bd8c9901bc888
juanudk <juanudkp@gmail.com>
2021-11-22 09:53:27 -0300

    Add environment and change to production

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { PoChartSerie, PoMenuItem } from '@po-ui/ng-components';
+import { environment } from '../environments/environment.prod';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,8 @@ import { PoChartSerie, PoMenuItem } from '@po-ui/ng-components';
 })
 export class AppComponent {
   public chartData: Array<PoChartSerie> = [];
-  public flag:boolean = false;
+  public items: Array<Object> = [];
+  public flag: boolean = false;
   readonly menus: Array<PoMenuItem> = [
     { label: 'Home', action: this.onClick.bind(this) }
   ];
@@ -19,15 +21,27 @@ export class AppComponent {
   }
   constructor(private httpClient: HttpClient) { }
   ngOnInit() {
-    this.httpClient.get<any>(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=<YOUR_API_KEY>`)
-    .subscribe(cryptos =>{
-      for (let element of cryptos.data) {
-          this.chartData.push({label: element.name,data: +element.quote.USD.market_cap});
+    this.httpClient.get<any>(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=`+environment.API_KEY_CMC)
+      .subscribe(cryptos => {
+        let i: number = 0;
+        for (let element of cryptos.data) {
+          if (i < 10) {
+            console.log(element)
+            this.chartData.push({ label: element.name, data: element.quote.USD.market_cap });
+            i++;
+          }
+          this.items.push({ rank: element.cmc_rank, name: element.name, 
+                          "24h%": element.quote.USD.percent_change_24h.toFixed(2).toString() + "%", 
+                          "7d%": element.quote.USD.percent_change_7d.toFixed(2).toString() + "%", 
+                          price: element, "Market Cap": parseInt(element.quote.USD.market_cap), 
+                          "Volume(24h)": parseInt(element.quote.USD.volume_24h), 
+                          "Circulating Supply": parseInt(element.circulating_supply) });
         }
-      this.flag = true;
+        console.log(this.chartData);
+        this.flag = true;
       }
-    );
-    
-    
+      );
+
+
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  API_KEY_CMC: "YOUR_API_KEY"
 };
