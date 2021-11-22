27ba0564cb9afbac15f1cbaf2470ab72ba717e62
juanudk <juanudkp@gmail.com>
2021-11-22 14:33:44 -0300

    Add feature to work with process.env

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { PoChartSerie, PoMenuItem } from '@po-ui/ng-components';
-import { environment } from '../environments/environment.prod';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
